6683c0f6c4718c1c32315c57a34406cadf9ffb8e
abdulmalik alsufayran <alsufiran@gmail.com>
2022-05-28 19:27:47 +0300

    updated translations front page

--- resources/views/livewire/car-wordle.blade.php
+++ resources/views/livewire/car-wordle.blade.php
@@ -114,52 +114,52 @@
                                 </svg>
                             </div>
                             <div class="mt-3 text-center sm:mt-5">
-                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">You are correct</h3>
+                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">{{ __('You are correct') }}</h3>
                                 <div class="mt-2">
-                                    <p class="text-sm text-gray-500">The car was indeed {{ $car->name }}. come back tomorrow for a new car and a new challenge</p>
-                                </div>
+                                    <p class="text-sm text-gray-500">{{ __('The car was indeed :carName. come back tomorrow for a new car and a new challenge', ['carName' => app()->getLocale() === 'ar' ? $car->name_ar : $car->name ]) }}</p>
                             </div>
                         </div>
-                        <div class="mt-5 sm:mt-6">
-                            <a href="/" class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm">Homepage</a>
-                        </div>
+                    </div>
+                    <div class="mt-5 sm:mt-6">
+                        <a href="/" class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm">{{ __('Homepage') }}</a>
                     </div>
                 </div>
             </div>
         </div>
-        @endif
-
-        <!-- failed modal -->
-        @if($failed)
-        <!-- This example requires Tailwind CSS v2.0+ -->
-        <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
-            <div class="fixed z-10 inset-0 overflow-y-auto">
-                <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-                    <div class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
-                        <div>
-                            <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
-                                <!-- Heroicon name: outline/check -->
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                                </svg>
-                            </div>
-                            <div class="mt-3 text-center sm:mt-5">
-                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">You failed this challenge</h3>
-                                <div class="mt-2">
-                                    <p class="text-sm text-gray-500">You should refresh the page and try again</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="mt-5 sm:mt-6">
-                            <a href="/" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm">Try again</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        @endif
     </div>
+    @endif
+
+    <!-- failed modal -->
+    @if($failed)
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div class="fixed z-10 inset-0 overflow-y-auto">
+            <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+                <div class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
+                    <div>
+                        <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                            <!-- Heroicon name: outline/check -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                            </svg>
+                        </div>
+                        <div class="mt-3 text-center sm:mt-5">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">{{ __('You failed this challenge') }}</h3>
+                            <div class="mt-2">
+                                <p class="text-sm text-gray-500">{{ __('You should refresh the page and try again') }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-5 sm:mt-6">
+                        <a href="/" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm">{{ __('Try again') }}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endif
+</div>
 </div>
